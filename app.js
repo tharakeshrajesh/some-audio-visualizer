d21af93073e18d0fcd07e49bd98d611054fc1286
tharakeshrajesh <tharakeshrajesh3@gmail.com>
2025-09-05 18:21:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
-let ctx, analyser, data, bgstate
+let ctx, analyser, data, bgstate, visualizing, perms, stream
 const bgstates = ["bass", "mid", "high", "off"]
 bgstate = 0
+visualizing = false
+perms = false
 
 alert("If you go blind or something bad happens to you from watching this then this is not my fault. This is your warning so beware.")
 
+document.getElementById('vsettingsui').style.display = 'none'
+
 document.getElementById('start').addEventListener('click', async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
-    ctx = new (window.AudioContext || window.webkitAudioContext)()
-    const src = ctx.createMediaStreamSource(stream)
-    analyser = ctx.createAnalyser()
-    analyser.fftSize = 1024
-    src.connect(analyser)
-    analyser.smoothingTimeConstant = 0.75
-    data = new Uint8Array(analyser.frequencyBinCount)
-    loop()
+    if (visualizing) {
+        document.getElementById('start').innerHTML = "Start visualizer"
+        return visualizing = false
+    }
+    try {
+        if (!perms) {
+            stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+        }
+        ctx = new (window.AudioContext || window.webkitAudioContext)()
+        const src = ctx.createMediaStreamSource(stream)
+        analyser = ctx.createAnalyser()
+        analyser.fftSize = 1024
+        src.connect(analyser)
+        analyser.smoothingTimeConstant = 0.75
+        data = new Uint8Array(analyser.frequencyBinCount)
+        document.getElementById('start').innerHTML = "Stop visualizer"
+        visualizing = true
+        perms = true
+        loop()
+    } catch (err) {
+        perms = false
+        alert("Mic permissions needed to visualize audio input")
+    }
 })
 
 document.getElementById("bgtoggle").addEventListener('click', () => {
     bgstate + 1 == 4 ? bgstate = 0 : bgstate += 1
     document.getElementById("bgtoggle").innerHTML = `Background reaction toggle - ${bgstates[bgstate]}`
+})
+
+document.getElementById('vsettings').addEventListener('click', () => {
+  document.getElementById('vsettingsui').style.display = document.getElementById('vsettingsui').style.display === 'none' ? 'flex' : 'none';
+})
+
+document.getElementById('setcolor').addEventListener('click', () => {
+    document.getElementById('bass').setAttribute('style', `background-color: ${document.getElementById('basscolor').value}`)
+    document.getElementById('mid').setAttribute('style', `background-color: ${document.getElementById('midcolor').value}`)
+    document.getElementById('high').setAttribute('style', `background-color: ${document.getElementById('highcolor').value}`)
 })
 
 function band(db, source, destination) {
@@ -38,8 +66,43 @@ function colors(b, m, h) {
     return [c1, c2, c3]
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function defaultthingy(bass, mid, high, v) {
+    bass = parseInt(bass.slice(0, -2), 10)
+    mid = parseInt(mid.slice(0, -2), 10)
+    high = parseInt(high.slice(0, -2), 10)
+
+    while (bass > 0 && visualizing === false) {
+        if (bass < 5) bass = 0;
+
+        document.getElementById('bass').style.height = `${bass}px`
+        document.getElementById('bass').style.width = `${bass}px`
+
+        document.getElementById('mid').style.width = `${mid}px`
+        document.getElementById('mid').style.height = `${mid}px`
+
+        document.getElementById('high').style.height = `${high}px`
+        document.getElementById('high').style.width = `${high}px`
+
+        document.body.style.background = `hsl(200, 80%, ${Math.max(0, v)}%)`
+
+        if (mid > 0) mid -= 2;
+        if (high > 0) high -= 2;
+        if (v > 0) v -= 1.5;
+
+        if (mid < 0) mid = 0;
+        if (high < 0) high = 0;
+
+        bass -= 2
+
+        await sleep(2);
+    }
+}
+
 function loop() {
-    requestAnimationFrame(loop)
     analyser.getByteFrequencyData(data)
     const b = band(data, 20, 160)
     const m = band(data, 160, 2000)
@@ -59,7 +122,9 @@ function loop() {
            255 800
         We would basically cross-multiply to get 800b = 255x. If this doesn't make sense then that sucks because I'm done teaching. I need to finish homework so no more documentation today.
     */ 
-    
+
+    if (!visualizing) return defaultthingy(bass, mid, high, bgstate == 0 ? b : bgstate == 1 ? m : bgstate == 2 ? h : 2 / 2);
+    requestAnimationFrame(loop)
 
     document.getElementById('bass').style.height = bass
     document.getElementById('bass').style.width = bass
